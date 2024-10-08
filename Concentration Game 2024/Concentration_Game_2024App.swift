95252d386884a1384babac24940a742853177dd6
Steve Liddle <liddle@byu.edu>
2024-10-08 10:10:44 -0600

    Initial Commit

---
+++ Concentration Game 2024/Concentration_Game_2024App.swift
@@ -0,0 +1,17 @@
+//
+//  Concentration_Game_2024App.swift
+//  Concentration Game 2024
+//
+//  Created by Stephen Liddle on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Concentration_Game_2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Concentration Game 2024/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Concentration Game 2024
+//
+//  Created by Stephen Liddle on 10/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
